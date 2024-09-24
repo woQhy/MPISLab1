a2328e8294e381b76cc1f3c8b834e4c179736870
Exage <horkavchuk.nik@gmail.com>
2024-09-24 18:28:49 +0300

    first commit

---
+++ app/src/main/java/com/example/mpistask01/model/Image.java
@@ -0,0 +1,29 @@
+package com.example.mpistask01.model;
+
+public class Image {
+    private String id;
+    private Urls urls;
+
+    public Urls getUrls() {
+        return urls;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static class Urls {
+        private String regular;
+        private String small;
+
+        public String getRegular() {
+            return  regular;
+        }
+
+        public String getSmall() {
+            return  small;
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/mpistask01/model/ImageResponse.java
@@ -0,0 +1,11 @@
+package com.example.mpistask01.model;
+import java.util.List;
+
+
+public class ImageResponse {
+    private List<Image> results;
+
+    public List<Image> getResults() {
+        return results;
+    }
+}
